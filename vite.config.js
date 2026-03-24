578549573a32f37e11fc95781bd8ce084ad9572c
Feinp51 <piminsona875@gmail.com>
2026-03-24 19:42:03 +0300

    fix base

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: './',
+  base: '/Bakhroma_Homestaging/',
+
   build: {
     outDir: 'dist',
     emptyOutDir: true,
